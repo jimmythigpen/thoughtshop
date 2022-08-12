a76832122c549a122fd9caeea5af9b6ca8f9ca67
Jimmy Thigpen <jmthigpen@me.com>
2022-08-12 14:24:18 -0600

    add netlify plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 /// <reference types="vitest" />
 import {defineConfig} from 'vite';
 import hydrogen from '@shopify/hydrogen/plugin';
+import netlifyPlugin from '@netlify/hydrogen-platform/plugin';
 
 export default defineConfig({
-  plugins: [hydrogen()],
+  plugins: [hydrogen(), netlifyPlugin()],
   resolve: {
     alias: [{find: /^~\/(.*)/, replacement: '/src/$1'}],
   },
